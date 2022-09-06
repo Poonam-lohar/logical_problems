c63102d0d82e2c840af427177fb7210e98562e5f
Your Name <you@example.com>
2022-09-06 11:03:18 +0530

    added:logic for calculate LCM

---
+++ src/PerfectSquare.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+    public class PerfectSquare {
+        static boolean Perfectsquare(double num)
+        {
+            double sqrt=Math.sqrt(num);
+            return ((sqrt-Math.floor(sqrt))==0);
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            double num = sc.nextDouble();
+            if (Perfectsquare(num))
+                System.out.println("Number is perfect square");
+            else
+                System.out.println("not perfect square");
+        }
+    }
